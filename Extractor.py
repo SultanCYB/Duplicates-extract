5d19e3b520e24a0dc5ca5281883334e1118af4cb
Sultan Bazher <107263975+SultanCYB@users.noreply.github.com>
2023-05-03 20:28:09 +0300

    Create Extractor.py

---
+++ Extractor.py
@@ -0,0 +1,8 @@
+def extractor(to_extract: "list or tuple"):
+    ''' For extracting all instances of the duplicated element'''
+    
+    done = dict()
+    for i in to_extract:
+        done[i] = done.get(i, 0) + 1
+    
+    return [ value for value, count in done.items() if count > 1 ]
